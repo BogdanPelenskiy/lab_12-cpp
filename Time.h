bae4e4e5ed52a1fd3bcbc016b9dbd502cf79374a
BogdanPelenskiy <80599403+BogdanPelenskiy@users.noreply.github.com>
2025-11-24 09:19:31 +0200

    Enhance Time class with new methods and comments
    Refactor Time class to include cyclicDistance method and update comments.

--- Time.h
+++ Time.h
@@ -1,25 +1,29 @@
 #ifndef TIME_H
 #define TIME_H
+
 #include <iostream>
 
 class Time {
 private:
-    int hours;
-    int minutes;
-    int seconds;
-    void normalize();
+int hours;
+int minutes;
+int seconds;
+
+void normalize(); // нормалізація у межах 0–86399
 
 public:
-    Time();
-    Time(int h, int m, int s);
-    ~Time() {}
+Time();
+Time(int h, int m, int s);
+~Time() {}
 
-    int toSeconds() const;
-    bool operator>(const Time& other) const;
-    Time& operator-=(int sec);
+int toSeconds() const; // у секунди (0–86399)
+int cyclicDistance() const; // мінімальна відстань до 0 за годинниковою логікою
 
-    friend std::ostream& operator<<(std::ostream& out, const Time& t);
-    friend std::istream& operator>>(std::istream& in, Time& t);
+bool operator>(const Time& other) const; // циклічне порівняння
+Time& operator-=(int sec); // циклічне зменшення
+
+friend std::ostream& operator<<(std::ostream& out, const Time& t);
+friend std::istream& operator>>(std::istream& in, Time& t);
 };
 
 #endif
